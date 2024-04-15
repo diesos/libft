4618878b3354ec8132c121118fec7b48ed628c4b
Diesos <ozturk@live.Fr>
2024-04-15 17:00:32 +0200

    Add ft_split and ft_strtrim functions

---
+++ ft_strtrim.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omeoztur <omeoztur@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/14 17:38:33 by omeoztur          #+#    #+#             */
+/*   Updated: 2024/04/15 16:57:21 by omeoztur         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	check_char(char const c, char const *set)
+{
+	size_t	i;
+
+	i = -1;
+	if (!set)
+		return (-1);
+	while (set[++i])
+	{
+		if (set[i] == c)
+			return (1);
+	}
+	return (0);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*trim;
+	size_t	start;
+	size_t	end;
+	size_t	i;
+
+	i = -1;
+	start = 0;
+	end = ft_strlen(s1);
+	while (s1[start] && check_char(s1[start], set))
+		start++;
+	while ((end > start) && check_char(s1[end - 1], set))
+		end--;
+	trim = malloc(sizeof(char) * (end - start) + 1);
+	if (!trim)
+		return (NULL);
+	while (end > start)
+	{
+		trim[++i] = s1[start];
+		start++;
+	}
+	trim[++i] = '\0';
+	return (trim);
+}
+// int	main(void)
+// {
+// 	char	str[] = "    Hello World       ";
+// 	char	set[] = " ";
+// 	char	str2[] = "--Hello-Bg---";
+// 	char	set2[] = "-";
+// 	char	str3[10];
+// 	char	set3[] = "-";
+
+// 	printf("%s\n", ft_strtrim(str, set));
+// 	printf("%s\n", ft_strtrim(str2, set2));
+// 	printf("%s\n", ft_strtrim(str3, set3));
+// 	return (0);
+// }
--- libft.h
+++ libft.h
@@ -40,5 +40,7 @@ void	*ft_calloc(size_t count, size_t size);
 // <------------------- ADDITIONAL --------------------->
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
 
 #endif
