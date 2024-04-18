3e3c5c30ed6e7a940fec1429d7c6db439d4c332a
Diesos <ozturk@live.Fr>
2024-04-18 15:35:42 +0200

    Add ft_putendl_fd function to write a string followed by a newline character to a file descriptor

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -1,20 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: omeoztur <omeoztur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/18 15:27:58 by omeoztur          #+#    #+#             */
-/*   Updated: 2024/04/18 15:34:48 by omeoztur         ###   ########.fr       */
+/*   Created: 2024/04/18 15:30:02 by omeoztur          #+#    #+#             */
+/*   Updated: 2024/04/18 15:35:23 by omeoztur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putstr_fd(char *s, int fd)
+void	ft_putendl_fd(char *s, int fd)
 {
 	if (!s)
 		return ;
-	write(fd, s, ft_strlen(s));
+	ft_putstr_fd(s, fd);
+	ft_putchar_fd('\n', fd);
 }
