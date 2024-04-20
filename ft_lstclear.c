1dbe415cc3261e6f0ecc7a6dd6164c5d168b1154
Omer Ozturk <ozturk@live.fr>
2024-04-21 00:48:07 +0200

    Update typo

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,13 +6,13 @@
 /*   By: omeoztur <omeoztur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 00:31:35 by omeoztur          #+#    #+#             */
-/*   Updated: 2024/04/21 00:46:32 by omeoztur         ###   ########.fr       */
+/*   Updated: 2024/04/21 00:47:56 by omeoztur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    ft_lstclear(t_list **lst, void (*del)(void
+void	ft_lstclear(t_list **lst, void (*del)(void
 *))
 {
 	if (!lst || !del)
