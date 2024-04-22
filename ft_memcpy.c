0a28e2d7fc435a0000ab108121065a1b7046c1b7
Omer Ozturk <ozturk@live.fr>
2024-04-22 13:38:06 +0200

    Update ft_memcpy function signature

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,13 +6,13 @@
 /*   By: omeoztur <omeoztur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 12:47:30 by omeoztur          #+#    #+#             */
-/*   Updated: 2024/04/22 10:58:31 by omeoztur         ###   ########.fr       */
+/*   Updated: 2024/04/22 10:59:49 by omeoztur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 {
 	size_t	i;
 	char	*dest;
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: omeoztur <omeoztur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 16:21:14 by omeoztur          #+#    #+#             */
-/*   Updated: 2024/04/22 10:04:42 by omeoztur         ###   ########.fr       */
+/*   Updated: 2024/04/22 11:00:01 by omeoztur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ void				*ft_memset(void *b, int c, size_t len);
 void				ft_bzero(void *s, size_t n);
 void				*ft_memmove(void *dst, const void *src, size_t len);
 void				*ft_memchr(const void *s, int c, size_t n);
-void				ft_memcpy(void *restrict dst, const void *restrict src,
+void				*ft_memcpy(void *restrict dst, const void *restrict src,
 						size_t n);
 int					ft_memcmp(const void *s1, const void *s2, size_t n);
 // <---------------------- is ---------------------->
