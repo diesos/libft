db3438607fc1aa97ef457b135d57336c849bc1fe
Diesos <ozturk@live.Fr>
2024-04-23 00:22:06 +0200

    Fix bug in ft_memcpy function and update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: omeoztur <omeoztur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 12:47:30 by omeoztur          #+#    #+#             */
-/*   Updated: 2024/04/23 00:15:26 by omeoztur         ###   ########.fr       */
+/*   Updated: 2024/04/23 00:20:35 by omeoztur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 {
-	int					i;
+	size_t				i;
 	unsigned char		*dest;
 	const unsigned char	*source;
 
 	i = 0;
-	dest = (char *)dst;
-	source = (const char *)src;
+	dest = (unsigned char *)dst;
+	source = (const unsigned char *)src;
 	while (i < n)
 	{
 		dest[i] = source[i];
