751fb56ae023ff0475f5f5d3cb5ac487ec60e489
Diesos <ozturk@live.Fr>
2024-04-22 23:52:42 +0200

    Fix bug in ft_memset function and update ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -6,25 +6,25 @@
 /*   By: omeoztur <omeoztur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/31 16:58:56 by omeoztur          #+#    #+#             */
-/*   Updated: 2024/04/09 12:50:23 by omeoztur         ###   ########.fr       */
+/*   Updated: 2024/04/22 23:52:10 by omeoztur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *ptr, int c, size_t len)
+void	*ft_memset(void *b, int c, size_t len)
 {
 	size_t	i;
 
 	i = 0;
-	if (ptr)
+	if (b)
 	{
 		while (i < len)
 		{
-			((unsigned char *)ptr)[i] = (unsigned char)c;
+			((unsigned char *)b)[i] = (unsigned char)c;
 			i++;
 		}
-		return ((unsigned char *)ptr);
+		return ((unsigned char *)b);
 	}
 	return (NULL);
 }
