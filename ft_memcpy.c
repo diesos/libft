2c5ad82de77480d1b87d00a69d0b397d78f22168
Omer Ozturk <ozturk@live.fr>
2024-04-22 10:57:54 +0200

    Update ft_memcpy function

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: omeoztur <omeoztur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 12:47:30 by omeoztur          #+#    #+#             */
-/*   Updated: 2024/04/22 10:17:31 by omeoztur         ###   ########.fr       */
+/*   Updated: 2024/04/22 10:57:32 by omeoztur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,14 @@ int	ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 {
 	size_t	i;
 	char	*dest;
-	char	*source;
 
 	i = 0;
 	dest = (char *)dst;
-	source = (const char *)src;
-	if (!dst && !src)
-		return (0);
 	while (i < n)
 	{
-		dest[i] = source[i];
+		*(char *)dest = *(char *)src;
+		dest++;
+		src++;
 		i++;
 	}
 	return (dst);
