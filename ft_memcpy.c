d23862c7e8bd24c46d00afe8a2c0205c8e0844f3
Diesos <ozturk@live.Fr>
2024-04-23 00:16:01 +0200

    Update ft_memcpy function to fix bug in copying logic

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: omeoztur <omeoztur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 12:47:30 by omeoztur          #+#    #+#             */
-/*   Updated: 2024/04/22 18:26:03 by omeoztur         ###   ########.fr       */
+/*   Updated: 2024/04/23 00:15:26 by omeoztur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,17 @@
 
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 {
-	size_t		i;
-	char		*dest;
-	const char	*source;
+	int					i;
+	unsigned char		*dest;
+	const unsigned char	*source;
 
+	i = 0;
 	dest = (char *)dst;
 	source = (const char *)src;
-	i = -1;
-	while (++i < n)
+	while (i < n)
+	{
 		dest[i] = source[i];
+		i++;
+	}
 	return (dst);
 }
