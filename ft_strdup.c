ea4bce7d665fdcf593ab66d050cdc6896b7f86f3
Diesos <ozturk@live.Fr>
2024-04-25 23:29:24 +0200

    Update ft_strdup function to handle NULL source string

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: omeoztur <omeoztur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 17:08:01 by omeoztur          #+#    #+#             */
-/*   Updated: 2024/04/22 17:46:08 by omeoztur         ###   ########.fr       */
+/*   Updated: 2024/04/25 23:29:10 by omeoztur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ char	*ft_strdup(const char *src)
 {
 	char	*str;
 
+	if (!src)
+		return (NULL);
 	str = (char *)malloc(sizeof(char) * (ft_strlen(src) + 1));
 	if (!str)
 		return (NULL);
